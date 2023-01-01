78533039520ffb521739732ad259426598b74dbb
Jeff Strom <jeff@pulsemedica.com>
2023-01-01 14:41:26 -0700

    merge files for index/app

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App";
+import { useAudioRecorder, AudioRecorder } from "react-audio-voice-recorder";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+  const recorderControls = useAudioRecorder();
+  const addAudioElement = blob => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement("audio");
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+  };
+
+  return (
+    <div>
+      <AudioRecorder
+        onRecordingComplete={blob => addAudioElement(blob)}
+        recorderControls={recorderControls}
+      />
+      <br />
+      <button onClick={recorderControls.stopRecording}>Stop recording</button>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root"),
+);
