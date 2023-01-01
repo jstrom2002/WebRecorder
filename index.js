0ea0429817bf859c9c1cbc05768d6feeb4d3b348
Jeff Strom <jeff@pulsemedica.com>
2023-01-01 13:06:16 -0700

    revert react dom render to old style as a fix

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/component/App";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <App />
-  </Router>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
