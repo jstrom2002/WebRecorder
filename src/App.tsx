e68b1242be81110b94b489eb60afc7032efec26c
jstrom2002 <jhstrom1@gmail.com>
2023-01-06 07:50:57 -0700

    flesh out UI, add state update for screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,107 @@ import LoginPage from "./screens/LoginPage";
 import MainPage from "./screens/MainPage";
 import RegisterPage from "./screens/RegisterPage";
 import { useState } from "react";
+import {
+  AppShell,
+  Burger,
+  Button,
+  Header,
+  Text,
+  MediaQuery,
+  Menu,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconUserCircle } from "@tabler/icons";
 
 export default function App() {
-  async function loginCallback() {
+  async function loginCallback(logState: boolean) {
     if (true) {
-      setLopggedIn(true);
+      setLoggedIn(logState);
     }
   }
 
-  const [loggedIn, setLopggedIn] = useState(false);
-  const [onRegisterPage, setOnRegisterPage] = useState(false);
+  async function registerCallback() {
+    if (true) {
+      setOnRegisterPage(true);
+    }
+  }
 
-  return loggedIn ? (
-    <MainPage />
-  ) : onRegisterPage ? (
-    <RegisterPage />
-  ) : (
-    <LoginPage />
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [onRegisterPage, setOnRegisterPage] = useState(false);
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+
+  function SelectScreen() {
+    return loggedIn ? (
+      <MainPage />
+    ) : onRegisterPage ? (
+      <RegisterPage />
+    ) : (
+      <LoginPage loginHandler={() => loginCallback(true)} />
+    );
+  }
+
+  return (
+    <>
+      <AppShell
+        layout="alt"
+        style={{
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        }}
+        header={
+          <Header height={{ base: 50, md: 70 }} p="md">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: "100%",
+                justifyContent: "right",
+              }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+
+              <Text>Web Recorder</Text>
+
+              <Menu shadow="md" width={200}>
+                <Menu.Target>
+                  <Button
+                    component="a"
+                    href="#"
+                    variant="subtle"
+                    leftIcon={<IconUserCircle size={14} />}
+                  ></Button>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  <Menu.Item>Profile</Menu.Item>
+                  <Menu.Item>Settings</Menu.Item>
+                  <Menu.Item>
+                    <Button
+                      variant="subtle"
+                      onClick={() => loginCallback(false)}
+                    >
+                      Log Out
+                    </Button>
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </div>
+          </Header>
+        }
+      >
+        <SelectScreen />
+      </AppShell>
+    </>
   );
 }
--- src/screens/LoginPage.tsx
+++ src/screens/LoginPage.tsx
@@ -6,64 +6,68 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
-import { useState } from "react";
+import React, { useState } from "react";
+import { IconExternalLink } from "@tabler/icons";
 
-export default function LoginPage() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  async function loginCallback() {
-    if (true) {
-      setLoggedIn(true);
-    }
+export interface LoginPageProps {
+  loginHandler: any;
+}
+export default class LoginPage extends React.Component<LoginPageProps> {
+  constructor(props: LoginPageProps) {
+    super(props);
+    this.state = { loginHandler: props.loginHandler };
   }
 
-  return (
-    <>
-      <Flex
-        style={{
-          justifyContent: "center",
-          width: "50%",
-          minHeight: "50%",
-          margin: "17.5% 25%",
-          border: "solid",
-        }}
-        direction="column"
-      >
-        <Title align="center">Login</Title>
-        <TextInput
+  render() {
+    return (
+      <>
+        <Flex
           style={{
+            justifyContent: "center",
             width: "50%",
-            height: "50%",
-            margin: "0px 25%",
+            minHeight: "50%",
+            margin: "0% 25%",
+            border: "solid",
           }}
-          placeholder="User Name"
-          label="User Name"
-        />
-        <PasswordInput
-          style={{
-            width: "50%",
-            height: "50%",
-            margin: "0px 25%",
-          }}
-          placeholder="Password"
-          label="Password"
-          withAsterisk
-        />
-        <Flex style={{ justifyContent: "center" }}>
-          <Button variant="subtle" style={{ margin: "0px 20px" }}>
-            Register
-          </Button>
-          <Button variant="subtle" style={{ margin: "0px 20px" }}>
-            Forgot Password
-          </Button>
-          <Button
-            onClick={loginCallback}
-            style={{ width: "12em", margin: "0px 20px" }}
-          >
-            Login
-          </Button>
-          <br />
+          direction="column"
+        >
+          <Title align="center">Web Recorder</Title>
+          <TextInput
+            style={{
+              width: "50%",
+              height: "50%",
+              margin: "0px 25%",
+            }}
+            placeholder="User Name"
+            label="User Name"
+          />
+          <PasswordInput
+            style={{
+              width: "50%",
+              height: "50%",
+              margin: "0px 25%",
+            }}
+            placeholder="Password"
+            label="Password"
+            withAsterisk
+          />
+          <Flex style={{ justifyContent: "center" }}>
+            <Button variant="subtle" style={{ margin: "0px 20px" }}>
+              Register
+            </Button>
+            <Button variant="subtle" style={{ margin: "0px 20px" }}>
+              Forgot Password
+            </Button>
+            <Button
+              onClick={this.props.loginHandler}
+              style={{ width: "12em", margin: "0px 20px" }}
+            >
+              Login
+            </Button>
+            <br />
+          </Flex>
         </Flex>
-      </Flex>
-    </>
-  );
+      </>
+    );
+  }
 }
